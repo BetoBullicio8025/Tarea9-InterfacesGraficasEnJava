73bd59335e844f89e4c4d106eefe6d6a0496aa9d
BetoBullicio8025 <126055898+BetoBullicio8025@users.noreply.github.com>
2023-05-01 19:03:01 -0600

    Add files via upload

---
+++ src/IU/Principal.java
@@ -0,0 +1,48 @@
+package IU;
+
+import IU.Eventos.EventosRaton;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Principal extends Frame {
+    private Label LblNombre;
+    private TextField txtNombre;
+    private Button btnNombre;
+
+    public Principal(String title) throws HeadlessException {
+        super(title);
+        this.setLayout(new FlowLayout());
+        LblNombre = new Label("Nombre la niñita q mas amas: ");
+        this.add(LblNombre);
+        txtNombre = new TextField(30);
+        this.add(txtNombre);
+        btnNombre = new Button("Saludar");
+        //btnNombre.addMouseListener(new EventosRaton());
+        btnNombre.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println( txtNombre.getText() );
+                JOptionPane.showMessageDialog(null, "Edu te ama muchooo " + txtNombre.getText());
+
+            }
+        });
+
+        this.add(btnNombre);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        this.setVisible(true);
+        this.setSize(800, 600);
+
+    }
+
+}
---
+++ src/Modelo/Main.java
@@ -0,0 +1,10 @@
+package Modelo;
+
+import IU.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        Principal ventana1 = new Principal("Hola Mundo AWT");
+
+    }
+}
